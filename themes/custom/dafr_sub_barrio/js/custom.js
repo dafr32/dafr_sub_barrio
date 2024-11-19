56a04b6e33d0a75e5f84fd7cd19240ec5d0b8276
dafr <dafr32@gmail.com>
2024-11-19 17:48:54 +0100

    scrollr

--- themes/custom/dafr_sub_barrio/js/custom.js
+++ themes/custom/dafr_sub_barrio/js/custom.js
@@ -8,12 +8,6 @@
   'use strict';
 
 
-  Drupal.behaviors.aos_init = {
-    attach: function (context, settings) {
-    	AOS.init();
-    },
-  };
-
   Drupal.behaviors.dafr_barrio_sass = {
     attach: function(context, settings) {
 
--- themes/custom/dafr_sub_barrio/js/global.js
+++ themes/custom/dafr_sub_barrio/js/global.js
@@ -7,6 +7,18 @@
 
   'use strict';
 
+  Drupal.behaviors.libraries_init = {
+    attach: function (context, settings) {
+    	AOS.init();
+      skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+      });
+    },
+  };
+
   Drupal.behaviors.dafr_sub_barrio = {
     attach: function (context, settings) {
 
